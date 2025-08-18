278f2e7ca2cad1fc44ce478b55344a16bcebd8df
kkeec <krishnakeec@gmail.com>
2025-08-18 14:21:12 +0300

    export splitted

--- src/executor/builtins/builtin_export/case_withargs_export.c
+++ src/executor/builtins/builtin_export/case_withargs_export.c
@@ -1,68 +1,103 @@
 #include "../../../../includes/executor.h"
 
-int	case_withargs_export(char **args, t_env **env_list)
+char    *extract_key(char *str)
 {
-	int		i;
-	char	*key;
-	char	*value;
-	char	*equal_sign;
-	char	*existing;
-	char	*plus_equal;
-	int		status;
+    char *key;
+    char *equal;
+    char *plus_equal;
 
-	status = 0;
-	i = 1;
-	while (args[i])
-	{
-		key = NULL;
-		value = NULL;
-		equal_sign = ft_strchr(args[i], '=');
-		plus_equal = ft_strnstr(args[i], "+=", ft_strlen(args[i]));
-		if (plus_equal)
-			key = ft_substr(args[i], 0, plus_equal - args[i]);
-		else if (equal_sign)
-			key = ft_substr(args[i], 0, equal_sign - args[i]);
-		else
-			key = ft_strdup(args[i]);
-		if (!is_valid_key(key))
-		{
-			ft_printstderr("export: `");
-			ft_printstderr(key);
-			ft_printstderr("`: not a valid identifier\n");
-			status = 1;
-			free(key);
-			i++;
-			continue ;
-		}
-		if (plus_equal)
-		{
-			value = ft_strdup(plus_equal + 2);
-			char *newval = trimquotes(value);
-			existing = get_env_value(*env_list, key);
-			if (existing)
-			{
-				char *new_value = ft_strjoin(existing, newval);
-				update_env(env_list, key, new_value);
-				free(new_value);
-			}
-			else
-				update_env(env_list, key, newval);
-			free(value);
-		}
-		else if (equal_sign)
-		{
-			value = ft_strdup(equal_sign + 1);
-			char *newval = trimquotes(value);
-			update_env(env_list, key, newval);
-			free(value);
-		}
-		else
-		{
-			if (!get_env_value(*env_list, args[i]))
-				update_env(env_list, key, NULL);
-		}
-		free(key);
-		i++;
-	}
-	return (status);
+    key = NULL;
+    equal = ft_strchr(str, '=');
+    plus_equal = ft_strnstr(str, "+=", ft_strlen(str));
+    if (plus_equal)
+            key = ft_substr(str, 0, plus_equal - str);
+    else if (equal)
+            key = ft_substr(str, 0, equal - str);
+    else
+            key = ft_strdup(str);
+    return (key);
+    
+}
+void    print_err_key(char *key)
+{
+    ft_printstderr("export: `");
+    ft_printstderr(key);
+    ft_printstderr("`: not a valid identifier\n");
+}
+void    handle_plus_equal(char *plus_equal, char *key, t_env **env_list)
+{
+    char *value;
+    char *trimval;
+    char *existing;
+    char *newval;
+
+    value = ft_strdup(plus_equal + 2);
+    trimval = trimquotes(value);
+    existing = get_env_value(*env_list, key);
+    if (existing)
+    {
+        newval = ft_strjoin(existing, trimval);
+        update_env(env_list, key, newval);
+        free(newval);
+    }
+    else
+        update_env(env_list, key, trimval);
+    free(value);
+}
+
+void    handle_equal(char *equal, char *key, t_env **env_list)
+{
+    char *value;
+    char *newval;
+
+    value = ft_strdup(equal + 1);
+    newval = trimquotes(value);
+    update_env(env_list, key, newval);
+    free(value);
+}
+
+void    update_key_val(char *str, char *key, t_env **env_list)
+{
+    char *equal;
+    char *plus_equal;
+
+    equal = ft_strchr(str, '=');
+    plus_equal = ft_strnstr(str, "+=", ft_strlen(str));
+    if (plus_equal)
+    {
+        handle_plus_equal(plus_equal, key, env_list);
+    }
+    else if (equal)
+        handle_equal(equal, key, env_list);
+    else
+    {
+        if (!get_env_value(*env_list, str))
+            update_env(env_list, str, NULL);
+    }
+}
+
+int case_withargs_export(char **args, t_env **env_list)
+{
+    int i;
+    char  *key;
+    int status = 0;
+
+    i = 1;
+    while (args[i])
+    {
+        key = NULL;
+        key = extract_key(args[i]);        
+        if (!is_valid_key(args[i]))
+        {
+            print_err_key(args[i]);
+            status = 1;
+            free(key);
+            i++;
+            continue ;
+        }
+        update_key_val(args[i], key, env_list);
+        free(key);
+        i++;
+    }
+    return (status);
 }
